1f9cad1f47529d81262bf4c5ff8a388aa8baa972
benjamin Pfeiffer <benpf@scc-pi7.scc.bu.edu>
2024-09-18 18:24:38 -0400

    changes

--- main.R
+++ main.R
@@ -104,7 +104,7 @@ row_medians <- function(x) {
 #' [1] 1 4 7
 #' summarize_rows(m, mean)
 #' [1] 2 5 8
-summarize_rows <- function(x, fn, na.rm=FALSE) {
+summarize_rows <- function(x, fn, na.rm=TRUE) {
     apply(x, 1, function(row) fn(row, na.rm = na.rm))
 }
 
@@ -144,7 +144,7 @@ summarize_rows <- function(x, fn, na.rm=FALSE) {
 #' 2 -0.01574033 1.026951 -0.04725656 -2.967057 2.571608      112              70      0
 #' 3 -0.09040182 1.027559 -0.02774705 -3.026888 2.353087      130              54      0
 #' 4  0.09518138 1.030461  0.11294781 -3.409049 2.544992       90              72      0
-summarize_matrix <- function(x, na.rm=FALSE) {
+summarize_matrix <- function(x, na.rm=TRUE) {
   result<-t(apply(x,1,function(x){
     c(mean=mean(x,na.rm=na.rm),
     stdev=sd(x,na.rm=na.rm),
@@ -159,20 +159,37 @@ summarize_matrix <- function(x, na.rm=FALSE) {
   result_df <- as.data.frame(result)
   colnames(result_df) <-c("mean", "stdev", "median", "min", "max", "num_lt_0", "num_btw_1_and_5", "num_na")
   return(result_df)
-  }
+}
 # ------------ Helper Functions Used By Assignment, You May Ignore ------------
 sample_normal <- function(n, mean=0, sd=1) {
-    return(NULL)
+  set.seed(1337)
+  samples <- rnorm(n, mean=mean, sd=sd)
+  return(samples)
 }
 
 sample_normal_w_missing <- function(n, mean=0, sd=1, missing_frac=0.1) {
-    return(NULL)
+  set.seed(1337)
+  samples <- rnorm(n, mean=mean, sd=sd)
+  missing <- rbinom(length(samples), 1, missing_frac)==1
+  samples[missing] <- NA
+  return(samples)
 }
 
 simulate_gene_expression <- function(num_samples, num_genes) {
-    return(NULL)
+  set.seed(1337)
+  gene_exp <- matrix(
+    rnbinom(num_samples*num_genes, rlnorm(num_genes,meanlog = 3), prob=runif(num_genes)),
+    nrow=num_genes
+  )
+  return(gene_exp)
 }
 
 simulate_gene_expression_w_missing <- function(num_samples, num_genes, missing_frac=0.1) {
-    return(NULL)
+  gene_exp <- simulate_gene_expression(num_samples, num_genes)
+  missing <- matrix(
+    rbinom(num_samples*num_genes, 1, missing_frac)==1,
+    nrow=num_genes
+  )
+  gene_exp[missing] <- NA
+  return(gene_exp)
 }
